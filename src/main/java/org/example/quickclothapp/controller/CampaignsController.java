83b1b06e1177bfb2de5d2200ac5dd2b649a20fc5
Camilo Nossa <calejandro_noss@outlook.com>
2024-10-18 15:53:36 -0500

    📝 Update swagger docs

--- src/main/java/org/example/quickclothapp/controller/CampaignsController.java
+++ src/main/java/org/example/quickclothapp/controller/CampaignsController.java
@@ -1,13 +1,22 @@
 package org.example.quickclothapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.example.quickclothapp.exception.DataServiceException;
+import org.example.quickclothapp.model.Campaign;
 import org.example.quickclothapp.payload.response.MessageResponse;
 import org.example.quickclothapp.service.intf.ICampaignsService;
 import org.example.quickclothapp.service.intf.IClotheBankService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/application/campaigns")
@@ -20,6 +29,9 @@ public class CampaignsController {
     }
 
 
+    @Operation(summary = "Obtiene toda las campañas activas en el momento", description = "Obtiene todas las campañas activas en el momento")
+    @ApiResponse(responseCode = "200", description = "Lista de todas las campañas activas en el momento", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Campaign.class)))})
+    @ApiResponse(responseCode = "400", description = "El valor mensaje retorna el mensaje de error", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class))})
     @GetMapping("/get_active")
     public ResponseEntity<?> getActiveCampaigns() {
         try {
